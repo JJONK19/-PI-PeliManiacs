19f6f33418cd1f0ff03bb89de27de148bf1ad658
Javier Aguilar <69376553+JJONK19@users.noreply.github.com>
2024-07-24 22:56:29 -0600

    Conexion a la base de datos

--- Backend/app.py
+++ Backend/app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import os
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@db/peliculas')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    try:
+        result = db.session.execute(text('SELECT 1')).fetchone()
+        if result[0] == 1:
+            return "Conexión a la base de datos exitosa!"
+    except Exception as e:
+        return f"Error de conexión a la base de datos: {str(e)}"
+    return "Algo salió mal"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
